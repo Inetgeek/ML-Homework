0cb647309daf87695e95e0204621205104b67965
inetgeek <42563262+Inetgeek@users.noreply.github.com>
2022-11-20 19:54:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -43,30 +43,30 @@ if __name__ == '__main__':
 
     # 开始训练
     # Logistic模型
-    # l_theta_gd = l_train_set.func_gd(theta=l_theta, alpha=1e-3)  # 采用GD算法优化模型
+    l_theta_gd = l_train_set.func_gd(theta=l_theta, alpha=1e-3)  # 采用GD算法优化模型
     l_theta_sgd = l_train_set.func_sgd(theta=l_theta, alpha=1e-6)  # 采用SGD算法优化模型
 
     # Softmax模型
-    # s_theta_gd = s_train_set.func_gd(theta=s_theta, alpha=1e-4)  # 采用GD算法优化模型
+    s_theta_gd = s_train_set.func_gd(theta=s_theta, alpha=1e-4)  # 采用GD算法优化模型
     s_theta_sgd = s_train_set.func_sgd(theta=s_theta, alpha=1e-6)  # 采用SGD算法优化模型
 
     # Perceptron模型
-    # p_theta_gd = p_train_set.func_gd(theta=p_theta, alpha=3e-7)  # 采用GD算法优化模型
+    p_theta_gd = p_train_set.func_gd(theta=p_theta, alpha=3e-7)  # 采用GD算法优化模型
     p_theta_sgd = p_train_set.func_sgd(theta=p_theta, alpha=3e-7)  # 采用SGD算法优化模型
 
     # MultiPerceptron模型
-    # mp_theta_gd = mp_train_set.func_gd(theta=mp_theta, alpha=3e-7)  # 采用GD算法优化模型
-    mp_theta_sgd = mp_train_set.func_sgd(theta=mp_theta, alpha=1e-1)  # 采用SGD算法优化模型
+    mp_theta_gd = mp_train_set.func_gd(theta=mp_theta, alpha=3e-7)  # 采用GD算法优化模型
+    mp_theta_sgd = mp_train_set.func_sgd(theta=mp_theta, alpha=1e-4)  # 采用SGD算法优化模型
 
     # 打印结果
-    # print("l_theta_gd:", l_theta_gd[0], "loss:", l_theta_gd[1])
-    # print("l_theta_sgd:", l_theta_sgd[0], "loss:", l_theta_sgd[1])
-    # print("s_theta_gd:", s_theta_gd[0], "loss:", s_theta_gd[1])
-    # print("s_theta_sgd:", s_theta_sgd[0], "loss:", s_theta_sgd[1])
-    # print("p_theta_gd:", p_theta_gd[0], "loss:", p_theta_gd[1])
-    # print("p_theta_sgd:", p_theta_sgd[0], "loss:", p_theta_sgd[1])
-    # print("mp_theta_gd:", mp_theta_gd[0], "loss:", mp_theta_gd[1])
-    print("mp_theta_sgd:", mp_theta_sgd[0], "loss:", mp_theta_sgd[2])
+    print("l_theta_gd:", l_theta_gd[0], "loss:", l_theta_gd[1])
+    print("l_theta_sgd:", l_theta_sgd[0], "loss:", l_theta_sgd[1])
+    print("s_theta_gd:", s_theta_gd[0], "loss:", s_theta_gd[1])
+    print("s_theta_sgd:", s_theta_sgd[0], "loss:", s_theta_sgd[1])
+    print("p_theta_gd:", p_theta_gd[0], "loss:", p_theta_gd[1])
+    print("p_theta_sgd:", p_theta_sgd[0], "loss:", p_theta_sgd[1])
+    print("mp_theta_gd:", mp_theta_gd[0], "loss:", mp_theta_gd[1])
+    print("mp_theta_sgd:", mp_theta_sgd[0], "loss:", mp_theta_sgd[1])
 
     # 结果比较
     plt_loss(l_theta_sgd[2], x_label="epoch", y_label="loss", title="Logistic")
